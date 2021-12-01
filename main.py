026cd52f7adf578bd6f2b15ae8e815dae977946c
Kayum Shaikh <76610015+Abdulkayum242@users.noreply.github.com>
2021-12-01 11:59:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+## Importing Packages
+import pickle
+import streamlit as st
+from utils import *
+
+@st.cache
+def get_predictions(input_tokens, starts, k = 1.0):
+    n_gram_counts_list = pickle.load(open('C:/Users/vikas bisoi/PycharmProjects/WordPrdictions/en_counts.txt', 'rb'))
+    vocabulary = pickle.load(open('C:/Users/vikas bisoi/PycharmProjects/WordPrdictions/vocab.txt', 'rb'))
+    suggestion = get_suggestions(input_tokens, n_gram_counts_list, vocabulary, k=k, start_with = starts)
+    return suggestion
+
+## Page Title
+st.set_page_config(page_title = "SwiftKey Text Prediction",
+    page_icon = "💬")
+st.title("SwiftKey Text Prediction")
+st.markdown("---")
+
+## Sidebar
+st.sidebar.header("The Capstone project is done in collaboration with Swiftkey.")
+st.sidebar.markdown("---")
+st.sidebar.markdown("People are spending an increasing amount of time on their mobile devices.But typing on mobile devices can be a difficult task.SwiftKey builds a smart keyboard that makes it easier for people to type on their mobile devices. One cornerstone of their smart keyboard is predictive text models. When someone types you, the keyboard presents option for what the next word might be. For example, the word might be are.")
+
+## Input Fields
+sentence = st.text_input("Enter a sentence")
+st.subheader("Optional Inputs")
+starts = st.text_input("The starting letter of the expected next word")
+k = st.number_input("Enter smoothing factor k")
+
+tokenized = sentence.split()
+
+if st.button("Predict"):
+    suggestion = get_predictions(tokenized, starts, k)
+    st.write(suggestion[0])
+
+
+st.markdown("---")
+st.markdown("This project is developed by Students of Ramniranjan Jhunjhunwala College under guidence of professor Cyrus Lentin Sir.")
+
+
