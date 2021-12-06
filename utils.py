1741b14e6a013a9d75e70c4ba628f8560dcc6c5b
Kayum Shaikh <76610015+Abdulkayum242@users.noreply.github.com>
2021-12-06 17:59:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,154 +1,101 @@
-def get_suggestions(previous_tokens, n_gram_counts_list, vocabulary, k=1.0, start_with=None):
-    """
-    Loops over all n-gram counts and returns all suggestions
-    Parameters:
-        previous_tokens(list): Input Sentence (in the form of tokens)
-        n_gram_counts_list(list): List of all n-gram counts
-        vocabulary(list): List containing all the words of our vocabulary
-        k(float): The value of k-smoothing factor
-        start_with(str): First few letters of the next word
-    Returns:
-        Tuple of suggestions from the auto_complete function
-    """
+import re
+from nltk.tokenize import word_tokenize
+import random
+import nltk
+from collections import Counter, defaultdict
+import pickle
+from wordcloud import WordCloud, STOPWORDS
+import json
 
-    # See how many models we have
-    count = len(n_gram_counts_list)
 
-    # Empty list for suggestions
-    suggestions = []
+with open("cleaned_data.txt", "rb") as fp:   # Unpickling
+    cleaned_data = pickle.load(fp)
 
-    # IMP: Earlier "-1"
 
-    # Loop over counts
-    for i in range(count-1):
 
-        # get n and nplus1 counts
-        n_gram_counts = n_gram_counts_list[i]
-        nplus1_gram_counts = n_gram_counts_list[i+1]
+word_count={}
+for word in cleaned_data:
+    if word not in word_count:
+        word_count[word]=0
+    word_count[word]+=1    
+  
 
-        # get suggestions
-        suggestion = auto_complete(previous_tokens, n_gram_counts,
-                                    nplus1_gram_counts, vocabulary,
-                                    k=k, start_with=start_with)
-        # Append to list
-        suggestions.append(suggestion)
 
-    return suggestions
 
-def auto_complete(previous_tokens, n_gram_counts, nplus1_gram_counts, vocabulary, k=1.0, start_with=None):
-    """
-    Loops over all words and returns that which has the maximum probability
-    Parameters:
-        previous_tokens(list): Input Sentence (in the form of tokens)
-        n_gram_counts(dict): Dictionary of counts of n-grams
-        nplus1_gram_counts(dict): Dictionary of counts of (n+1)-grams
-        vocabulary(list): List containing all the words of our vocabulary
-        k(float): The value of k-smoothing factor
-        start_with(str): First few letters of the next word
-    Returns:
-        Tuple
-         - suggestion(str): The word with max probability
-         - max_prob(float): Corresponding probability
-    """
+counter={}
+for i in word_count.keys():
+    if i not in list(STOPWORDS):
+        counter[i]=word_count[i]
+print(len(counter.keys()))
 
-    # length of previous words
-    n = len(list(n_gram_counts.keys())[0])
 
-    # most recent 'n' words
-    previous_n_gram = previous_tokens[-n:]
 
-    # Calculate probabilty for all words
-    probabilities = probs(previous_n_gram,n_gram_counts, nplus1_gram_counts,vocabulary, k=k)
 
-    # Intialize the suggestion and max probability
-    suggestion = None
-    max_prob = 0
 
-    # Iterate over all words and probabilites, returning the max.
-    # We also add a check if the start_with parameter is provided
-    for word, prob in probabilities.items():
 
-        if start_with != None:
 
-            if not word.startswith(start_with):
-                continue
+def extra_space(text):
+    
+    new_text= re.sub("\s+"," ",text)
+    return new_text
+    
 
-        if prob > max_prob:
+def sp_charac(text):
+    
+    new_text=re.sub("[^0-9A-Za-z ]", "" , text)
+    return new_text
+    
 
-            suggestion = word
-            max_prob = prob
+def tokenize_text(text):
+    
+    new_text=word_tokenize(text)
+    return new_text
+    
+  
 
-    return suggestion, max_prob
+    
+  
+import numpy as np
+np.save('unigram_dict', word_count) 
+    
 
-def probs(previous_n_gram, n_gram_counts, nplus1_gram_counts, vocabulary, k=1.0) -> 'dict':
-  """
-  This function calculates the Probability for all the words in the vocabulary.
-  Parameters:
-      previous_n_gram(Sequence): A Sequence containing the previos n-gram
-      n_gram_counts(dict): Dictionary of counts of n-grams
-      nplus1_gram_counts(dict): Dictionary of counts of (n+1)-grams
-      vocabulary(list): List containing all the words of our vocabulary
-      k(float): The value of k-smoothing factor
-  Returns:
-      probabilites(dict): A Dictionary mapping from next words to the probability.
-  """
+ngram_dict1=defaultdict(lambda: defaultdict(lambda: 0))
 
-  # Convert to Tuple
-  previous_n_gram = tuple(previous_n_gram)
+with open('uni_dict.json', 'w') as fp:
+    json.dump(ngram_dict1, fp)
 
-  # Add end and unknown tokens to the vocabulary
-  vocabulary = vocabulary + ["<e>", "<unk>"]
+def gen_uni_keys(cleaned_data):
+    
+    for i in range(len(cleaned_data) - 1):
+        
+          yield [ cleaned_data[i], cleaned_data[i + 1] ]
 
-  # Calculate the size of the vocabulary
-  vocabulary_size = len(vocabulary)
+with open('uni_dict.json', 'r') as fp:
+    uni_dict=json.loads( fp.read())
 
-  # Empty dict for probabilites
-  probabilities = {}
 
-  # Iterate over words
-  for word in vocabulary:
 
-    # Calculate probability
-    probability = prob_for_single_word(word, previous_n_gram,
-                                           n_gram_counts, nplus1_gram_counts,
-                                           vocabulary_size, k=k)
-    # Create mapping: word -> probability
-    probabilities[word] = probability
+    
 
-  return probabilities
+def prob(counter):
+    
+    
+    for words in counter:
+        total_count = float(sum(counter[words].values()))
+        for nw in counter[words]:
+            counter[words][nw] = (counter[words][nw]+1)/(total_count+vocab)
+    
+    
 
-def prob_for_single_word(word, previous_n_gram, n_gram_counts, nplus1_gram_counts, vocabulary_size, k = 1.0) -> 'float':
-  """
-  Calculate Probability for the next word given the prior n-gram.
-  Parameters:
-      word(str): The next word
-      previous_n_gram(Sequence): A Sequence containing the previos n-gram
-      n_gram_counts(dict): Dictionary of counts of n-grams
-      nplus1_gram_counts(dict): Dictionary of counts of (n+1)-grams
-      vocabulary_size(int): The number of words in the vocabulary
-      k(float): The value of k-smoothing factor
-  Returns:
-      prob(float): The probability for the next word.
-  """
+def unipred(word):
+    
+   if word not in uni_dict.keys(): #if that word does not exist in our dictionary then we predict some random word
+        
+        preds=Counter(ngram_dict1[random.choice(list(counter.keys()))]).most_common()[:3]
+        print(preds)
+   else:
+        preds=Counter(uni_dict[word]).most_common()[:3]
+        print(preds)
+        
+     
 
-  # Convert the previous_n_gram into a tuple
-  previous_n_gram = tuple(previous_n_gram)
-
-  # Calculating the count, if exists from our freq dictionary otherwise zero
-  previous_n_gram_count = n_gram_counts[previous_n_gram] if previous_n_gram in n_gram_counts else 0
-
-  # The Denominator
-  denom = previous_n_gram_count + k * vocabulary_size
-
-  # previous n-gram plus the current word as a tuple
-  nplus1_gram = previous_n_gram + (word,)
-
-  # Calculating the nplus1 count, if exists from our freq dictionary otherwise zero
-  nplus1_gram_count = nplus1_gram_counts[nplus1_gram] if nplus1_gram in nplus1_gram_counts else 0
-
-  # Numerator
-  num = nplus1_gram_count + k
-
-  # Final Fraction
-  prob = num / denom
-  return prob
