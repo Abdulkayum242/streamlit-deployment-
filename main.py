ffcb1c85d6ec5cb95669d0eece4c617e4905d31f
Kayum Shaikh <76610015+Abdulkayum242@users.noreply.github.com>
2021-12-10 18:53:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,32 @@ import time
 import re
 from nltk.tokenize import word_tokenize
 from utils  import *
+import json
+import streamlit as st
 
 
 st.title('word prediction')
 st.balloons()
 
-
-
-def main_function(text): 
-      while(True):
-         cleaned_text=extra_space(text)
-         cleaned_text=sp_charac(cleaned_text)
-         tokenized=tokenize_text(cleaned_text)
-         if len(tokenized)==1:
-            ab=unipred(tokenized[0])
-            
-            break
-
-
 text= st.text_input("Enter a sentence")
 
+
+
+ #This function is used when we have only one preceding word 
+
+if text not in uni_dict.keys(): #if that word does not exist in our dictionary then we predict some random word
+        
+       preds=Counter(ngram_dict1[random.choice(list(uni_dict.keys()))]).most_common()
+      
+else:
+        preds=Counter(uni_dict[word]).most_common()[:3]
+
+
+
 if st.button("Predict"):
-    suggestion=main_function(text)
-    st.write(suggestion)
+    #sugg=unipred(text)
+    for i in preds:
+            st.write(i[0:5])
+
     st.write('Done!')
 
